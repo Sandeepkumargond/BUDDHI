4c0210c61ef428a3f5d8f57d079c5e9c462d0638
Aryan Kumar Arya <abcunofficialkumar@gmail.com>
2025-11-16 11:55:52 +0530

    about to start faculty part

--- client/src/lib/data.js
+++ client/src/lib/data.js
@@ -1,7 +1,7 @@
 // ===========================
 //        USER ROLE
 // ===========================
-export let role = "superadmin"; // admin | subadmin | superadmin | student
+export let role = "student"; // admin | subadmin | superadmin | student
 
 export const facultysData = [
   {
