3a17ba5e1f9bedc3428be58ca1f98f34798b802a
Sandeepkumargond <sandeepkumargond198@gmail.com>
2025-12-08 11:36:25 +0530

    error of rozerpay haas been fixed

--- server/models/razorpayCredential.model.js
+++ server/models/razorpayCredential.model.js
@@ -1,3 +1,46 @@
+import mongoose, { Schema } from "mongoose";
+
+const razorpayCredentialSchema = new Schema(
+	{
+		admin: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Admin",
+			required: true,
+			index: true,
+			unique: true,
+		},
+		keyIdEncrypted: {
+			type: String,
+			default: null,
+		},
+		keySecretEncrypted: {
+			type: String,
+			default: null,
+		},
+		webhookSecretEncrypted: {
+			type: String,
+			default: null,
+		},
+		mode: {
+			type: String,
+			enum: ["test", "live"],
+			default: "test",
+			lowercase: true,
+			trim: true,
+		},
+		createdBy: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Admin",
+		},
+		updatedBy: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Admin",
+		},
+	},
+	{ timestamps: true }
+);
+
+export const RazorpayCredential = mongoose.model("RazorpayCredential", razorpayCredentialSchema);
 
 
 
